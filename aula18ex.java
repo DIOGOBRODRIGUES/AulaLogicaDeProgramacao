c40d4fe3b15a65d53e73cf8a3bc2bd8d5de463c2
Diogo Rodrigues <diogo.borbar@gmail.com>
2021-01-04 14:59:29 -0300

    Ex18 completo

--- aula18ex.java
+++ aula18ex.java
@@ -7,13 +7,25 @@ public class aula18ex
         double altura = 2.10;
         String profissao = "músico";
 		boolean isLegal = true;
-		char gênero = 'm';
-		System.out.println("O nome é" + meuNome);
+        char gênero = 'm';
+
+        System.out.println("O nome é" + meuNome);
 		System.out.println("A idade " + minhaIdade);
         System.out.println("O peso é " + meuPeso);
         System.out.println("A Altura é " + altura);
         System.out.println("A profissão é " + profissao);
 		System.out.println("É legal? " + isLegal);
-		System.out.println("O gênero é " + gênero);
+        System.out.println("O gênero é " + gênero);
+        
+        int idadeDias = minhaIdade*365;
+        int idadeHoras = idadeDias*24;
+        int idadeMinutos = idadeHoras*60;
+        int idadeSegundos = idadeMinutos*60;
+
+        System.out.println("A sua idade em dias: "+idadeDias+", em horas "+idadeHoras+", em minutos "
+        + idadeMinutos+", em segundos "+idadeSegundos);
+        
+		
+        
     }
 }
